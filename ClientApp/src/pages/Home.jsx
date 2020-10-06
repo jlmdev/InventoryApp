6a091aac7f8db0d099bdb06006496f399d52298e
Josh Mann <jlmann72@gmail.com>
2020-10-06 18:26:28 -0400

    Links placed in home page buttons

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, Redirect, Route } from 'react-router-dom'
 
 export function Home() {
   return (
@@ -13,24 +14,36 @@ export function Home() {
         </nav>
       </div>
       <div className="button-group">
-        <button type="button" className="btn btn-primary btn-lg btn-block">
-          Workstations
-        </button>
-        <button type="button" className="btn btn-secondary btn-lg btn-block">
-          Servers
-        </button>
-        <button type="button" className="btn btn-success btn-lg btn-block">
-          Network
-        </button>
-        <button type="button" className="btn btn-danger btn-lg btn-block">
-          Device Users
-        </button>
-        <button type="button" className="btn btn-warning btn-lg btn-block">
-          Locations
-        </button>
-        <button type="button" className="btn btn-info btn-lg btn-block">
-          Users
-        </button>
+        <Link to="/workstations">
+          <button type="button" className="btn btn-primary btn-lg btn-block">
+            Workstations
+          </button>
+        </Link>
+        <Link to="/servers">
+          <button type="button" className="btn btn-secondary btn-lg btn-block">
+            Servers
+          </button>
+        </Link>
+        <Link to="/network-devices">
+          <button type="button" className="btn btn-success btn-lg btn-block">
+            Network
+          </button>
+        </Link>
+        <Link to="/device-users">
+          <button type="button" className="btn btn-danger btn-lg btn-block">
+            Device Users
+          </button>
+        </Link>
+        <Link to="/locations">
+          <button type="button" className="btn btn-warning btn-lg btn-block">
+            Locations
+          </button>
+        </Link>
+        <Link to="/users">
+          <button type="button" className="btn btn-info btn-lg btn-block">
+            Users
+          </button>
+        </Link>
       </div>
     </>
   )
