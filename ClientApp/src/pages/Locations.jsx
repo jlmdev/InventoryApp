7a6f51389c842a1ade3cc8ba2958a7a83bb36997
Josh Mann <jlmann72@gmail.com>
2020-10-06 14:38:00 -0400

    Created pages for remaining groups and single pages as copies of workstation pages

---
+++ ClientApp/src/pages/Locations.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export function Workstations() {
+  return (
+    <>
+      <div>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Workstations
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <div className="button-group">
+        <button type="button" className="btn btn-primary btn-lg btn-block">
+          Server
+        </button>
+        <button type="button" className="btn btn-secondary btn-lg btn-block">
+          Server
+        </button>
+      </div>
+    </>
+  )
+}
