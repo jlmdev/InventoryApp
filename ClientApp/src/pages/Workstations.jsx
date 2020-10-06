1c4d6e50b497c0485131bfef76b274015fd26b59
Josh Mann <jlmann72@gmail.com>
2020-10-06 13:49:02 -0400

    Added Workstations static template page

--- ClientApp/src/pages/Workstations.jsx
+++ ClientApp/src/pages/Workstations.jsx
@@ -6,30 +6,21 @@ export function Workstations() {
       <div>
         <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
             <li className="breadcrumb-item active" aria-current="page">
-              Home
+              Workstations
             </li>
           </ol>
         </nav>
       </div>
       <div className="button-group">
         <button type="button" className="btn btn-primary btn-lg btn-block">
-          Workstations
+          Search
         </button>
         <button type="button" className="btn btn-secondary btn-lg btn-block">
-          Servers
-        </button>
-        <button type="button" className="btn btn-success btn-lg btn-block">
-          Network
-        </button>
-        <button type="button" className="btn btn-danger btn-lg btn-block">
-          Device Users
-        </button>
-        <button type="button" className="btn btn-warning btn-lg btn-block">
-          Locations
-        </button>
-        <button type="button" className="btn btn-info btn-lg btn-block">
-          Users
+          Server
         </button>
       </div>
     </>
