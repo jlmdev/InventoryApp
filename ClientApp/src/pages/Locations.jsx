edbe5051991a3d7d6d5c066402fc6e60ec8c7ed9
Josh Mann <jlmann72@gmail.com>
2020-10-06 16:07:13 -0400

    Locations static page updated

--- ClientApp/src/pages/Locations.jsx
+++ ClientApp/src/pages/Locations.jsx
@@ -10,17 +10,20 @@ export function Locations() {
               <a href="#">Home</a>
             </li>
             <li className="breadcrumb-item active" aria-current="page">
-              Workstations
+              Locations
             </li>
           </ol>
         </nav>
       </div>
       <div className="button-group">
         <button type="button" className="btn btn-primary btn-lg btn-block">
-          Server
+          Search
+        </button>
+        <button type="button" className="btn btn-primary btn-lg btn-block">
+          Create New Location
         </button>
         <button type="button" className="btn btn-secondary btn-lg btn-block">
-          Server
+          Location
         </button>
       </div>
     </>
