9481631c00e2346e47f88a14db3a1917b41daba5
Josh Mann <jlmann72@gmail.com>
2020-10-07 14:46:20 -0400

    Added Workstation model

---
+++ Models/Workstation.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace InventoryApp.Models
+{
+    public class Workstation
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Serial { get; set; }
+        public DateTime DateAcquired { get; set; }
+        public string Description { get; set; }
+        public string Processor { get; set; }
+        public string OS { get; set; }
+        public bool Active { get; set; }
+        public string LastUpdate { get; set; }
+        public string Type { get; set; }
+    }
+}
