0a42b5555681c6ebf33d104122ccf3aedec9dea8
Josh Mann <jlmann72@gmail.com>
2020-10-11 11:32:43 -0400

    Created Servers page

--- ClientApp/src/pages/Servers.jsx
+++ ClientApp/src/pages/Servers.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export function Servers() {
+  // State declarations
+  const [servers, setServers] = useState([])
+  const [filterText, setFilterText] = useState('')
+
+  useEffect(
+    function () {
+      async function loadServers() {
+        const url =
+          filterText.length === 0
+            ? `/api/Servers`
+            : `/api/Servers?filter=${filterText}`
+        const response = await fetch(url)
+        const json = await response.json()
+        setServers(json)
+      }
+      loadServers()
+    },
+    [filterText]
+  )
+
   return (
     <>
       <div>
         <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
             <li className="breadcrumb-item">
-              <a href="#">Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li className="breadcrumb-item active" aria-current="page">
               Servers
@@ -17,21 +37,39 @@ export function Servers() {
         </nav>
       </div>
       <div className="button-group">
-        <Link to="/servers/single-server">
-          <button type="button" className="btn btn-primary btn-lg btn-block">
-            Search
-          </button>
-        </Link>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">
+              Search
+            </span>
+          </div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search Term"
+            aria-label="Search Term"
+            aria-describedby="basic-addon1"
+            value={filterText}
+            onChange={function (event) {
+              setFilterText(event.target.value)
+            }}
+          />
+        </div>
         <Link to="/servers/single-server">
           <button type="button" className="btn btn-primary btn-lg btn-block">
             Create New Server
           </button>
         </Link>
-        <Link to="/servers/single-server">
-          <button type="button" className="btn btn-secondary btn-lg btn-block">
-            Server
-          </button>
-        </Link>
+        {servers.map((server) => (
+          <Link key={server.id} to={`/servers/${server.id}`}>
+            <button
+              type="button"
+              className="btn btn-secondary btn-lg btn-block"
+            >
+              {server.name}
+            </button>
+          </Link>
+        ))}
       </div>
     </>
   )
